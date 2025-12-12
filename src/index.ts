aa2f0b76e632e701613957a90bfee439ec82d6b4
Denis Kruschinski <4989565+kruschid@users.noreply.github.com>
2025-12-13 00:07:27 +0100

    fix(components): components export fixed

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 // breadcrumbs
 export * from "@/components/breadcrumb/Breadcrumb";
 // buttons
+export * from "@/components/buttons/CloneButton";
 export * from "@/components/buttons/CreateButton";
 export * from "@/components/buttons/DeleteButton";
 export * from "@/components/buttons/EditButton";
+export * from "@/components/buttons/ExportButton";
+export * from "@/components/buttons/ImportButton";
 export * from "@/components/buttons/ListButton";
 export * from "@/components/buttons/RefreshButton";
 export * from "@/components/buttons/SaveButton";
 export * from "@/components/buttons/ShowButton";
+// uncategorized components
+export * from "@/components/ColorSchemeToggle";
 // crud
 export * from "@/components/crud/Create";
 export * from "@/components/crud/Edit";
@@ -23,21 +28,20 @@ export * from "@/components/fields/UrlField";
 // layout
 export * from "@/components/layout/Layout";
 // notification
+export * from "@/components/notification/AutoSaveIndicator";
 export * from "@/components/notification/Message";
 // table
 export * from "@/components/table/ColumnFilter";
 export * from "@/components/table/ColumnSorter";
 export * from "@/components/table/Table";
-// uncategorized components
-export * from "@/components/notification/AutoSaveIndicator";
-export * from "@/components/ColorSchemeToggle";
 // hooks
 export * from "@/hooks/useForm";
 export * from "@/hooks/useOtp";
 // pages
+export * from "@/pages/auth/ForgotPasswordPage";
 export * from "@/pages/auth/LoginPage";
 export * from "@/pages/auth/RegisterPage";
+export * from "@/pages/auth/UpdatePasswordPage";
 // providers
 export type * from "@/providers/authProvider";
 export * from "@/providers/notificationProvider";
-
